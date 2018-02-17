a95d346406204e921d486aef7481aec9f2891fb4
Martin Andersen <martin_haugaard8@hotmail.com>
2018-02-17 12:59:06 +0100

    Tilføjede mappe til tests af features. Indeholder test til fil browsing for billeder

---
+++ Feature test/ImageBrowsingTest/ImageBrowsingTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace ImageBrowsingTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ofdPicture = new OpenFileDialog();
+            ofdPicture.Filter = "Image files|*.bmp;*.jpg;*.gif;*.png;*.tif|All files|*.*";
+            ofdPicture.FilterIndex = 1;
+
+            if (ofdPicture.ShowDialog() == true)
+                image.Source = new BitmapImage(new Uri(ofdPicture.FileName));
+        }
+    }
+}
