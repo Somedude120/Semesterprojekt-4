5720062275c05e4b2e07e6456313afdcf5e37b5f
Martin Andersen <martin_haugaard8@hotmail.com>
2018-02-25 16:17:27 +0100

    Tilføjede projekt med fungerende mikrofon optager program. Implementering kan senere bruges til VoIP.

---
+++ Feature test/MicrophoneInput/MicrophoneInput/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+using NAudio.Mixer;
+
+namespace MicrophoneInput
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+         
+        }
+
+        public WaveIn waveSource = null;
+        public WaveFileWriter waveFile = null;
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            waveSource = new WaveIn();
+            waveSource.WaveFormat = new WaveFormat(44100, 1);
+
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+            waveFile = new WaveFileWriter(FileTextBox.Text, waveSource.WaveFormat);
+
+            waveSource.StartRecording();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            waveSource.StopRecording();
+        }
+
+        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveSource != null)
+            {
+                waveSource.Dispose();
+                waveSource = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+        }
+    }
+}
