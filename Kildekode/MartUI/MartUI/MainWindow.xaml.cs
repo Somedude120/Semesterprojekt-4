211bd2b0ee6776133bfca2c5fb9691ef788f706c
Søren Mathiasen <zuperzam@email.dk>
2018-02-28 19:49:13 +0100

    Added titlebar functionality to MartUI

--- Kildekode/MartUI/MartUI/MainWindow.xaml.cs
+++ Kildekode/MartUI/MartUI/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace MartUI
 {
@@ -20,9 +22,53 @@ namespace MartUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WindowState PrevState { get; set; }
+        private bool Maximized;
+        private double PrevLeft, PrevTop, PrevWidth, PrevHeight;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void ExitApplication(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MinimizeApplication(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ExpandApplication(object sender, RoutedEventArgs e)
+        {
+            if (Maximized == true)
+            {
+                this.Left = PrevLeft;
+                this.Top = PrevTop;
+                this.Width = PrevWidth;
+                this.Height = PrevHeight;
+                Maximized = false;
+            }
+            else
+            {
+                PrevLeft = this.Left;
+                PrevTop = this.Top;
+                PrevWidth = this.Width;
+                PrevHeight = this.Height;
+                this.Width = SystemParameters.WorkArea.Width;
+                this.Height = SystemParameters.WorkArea.Height;
+                this.Top = SystemParameters.WorkArea.Top;
+                this.Left = SystemParameters.WorkArea.Left;
+                Maximized = true;
+            }
         }
     }
 }
