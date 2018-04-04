893f3d55b1b411e138c7020f3147cdf50c637eb7
Alexander Davidsen <35957336+AlexBigD@users.noreply.github.com>
2018-04-04 20:31:48 +0200

    Tilføjede ændringer i MartoDatabasePrototype

---
+++ Feature test/MartoDatabasePrototype/MartoDatabasePrototype/dbo.Friends.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Friends] (
+    [Id]       INT           NOT NULL,
+    [Name]     NVARCHAR (50) NOT NULL,
+    [Username] NVARCHAR (50) NOT NULL,
+    [Tag]      NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- Feature test/MartoDatabasePrototype/MartoDatabasePrototype/formMain.cs
+++ Feature test/MartoDatabasePrototype/MartoDatabasePrototype/formMain.cs
@@ -7,29 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace MartoDatabasePrototype
 {
     public partial class formMain : Form
     {
+        SqlConnection connection;
+        string connectionString;
         public formMain()
         {
             InitializeComponent();
+
+            connectionsString = ConfigurationManager.ConnectionStrings["MartoDatabasePrototype.Properties.Settings.FriendlistConnectionString"].ConnectionString;
         }
 
-        private void friendBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.friendBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.friendlistDataSet);
-
-        }
 
         private void formMain_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'friendlistDataSet.Friend' table. You can move, or remove it, as needed.
-            this.friendTableAdapter.Fill(this.friendlistDataSet.Friend);
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PopulateFriendList()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Friends", connection))
+            {
+                connection.Open()
+            }
         }
     }
 }
