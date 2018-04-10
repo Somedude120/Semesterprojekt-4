e9b5b6c42b1de9e43a1c07cbac23503474f4036a
FatimaAU <fatima_kodro@hotmail.com>
2018-04-10 11:48:39 +0200

    Alt muligt

---
+++ Kildekode/MartUI-Features/MartUI/CreateUser/CreateUser.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MartUI.CreateUser
+{
+    /// <summary>
+    /// Interaction logic for CreateUserView.xaml
+    /// </summary>
+    public partial class CreateUser : UserControl
+    {
+        public CreateUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kildekode/MartUI-Features/MartUI/Helpers/Bootstrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace MartUI
+{
+    // Initialize all services
+    class Bootstrapper : UnityBootstrapper
+    {
+        //Returns instance of MainWindow 
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        //Need to override to be able to resolve types
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            // Register types
+            Container.RegisterType(typeof(object), typeof(Login.Login), "Login");
+            //Container.RegisterType(typeof(object), typeof(CreateUserView), "CreateUserView");
+        }
+
+
+
+    }
+}
