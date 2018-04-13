ca8f6b40d4a9e4aa13d6c99441ba394baff75df1
Daniel Hansen <dph1108@gmail.com>
2018-04-13 21:59:07 +0200

    Har lavet en kopi af async til test og prøve at lave featuren sha salty

---
+++ Feature test/AsyncServerHashing/AsyncSocket/AsyncClient/Class/StateObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace AsyncClient.Class
+{
+    // State object for receiving data from remote device.  
+    public class StateObject
+    {
+        // Client socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 256;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+}
