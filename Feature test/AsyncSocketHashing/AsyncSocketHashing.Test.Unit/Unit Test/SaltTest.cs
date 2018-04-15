c181bb392dc1aaec77ddcf3d11bc4dbe9137c2ae
DPH <DPH1108@gmail.com>
2018-04-15 21:40:11 +0200

    Lavede lige lidt mere på SaltTesten.

--- Feature test/AsyncSocketHashing/AsyncSocketHashing.Test.Unit/Unit Test/SaltTest.cs
+++ Feature test/AsyncSocketHashing/AsyncSocketHashing.Test.Unit/Unit Test/SaltTest.cs
@@ -39,8 +39,21 @@ namespace AsyncSocketHashing.Test.Unit.Unit_Test
             _uut1 = new SaltedHash("1234");
             _uut2 = new SaltedHash("1234");
 
-            bool notSame = _uut2.Verify(_uut1.Salt, _uut1.Hash, "1234");
-            Assert.AreNotEqual(_uut2, true);
+            Assert.AreNotEqual(_uut2, _uut1);
+        }
+
+        [Test]
+        public void Passwords_Are_Unique_Salt_Verify()
+        {
+            _uut1 = new SaltedHash("1234");
+            _uut2 = new SaltedHash("1234");
+
+            Console.WriteLine("Test1: " + _uut1.Hash);
+            Console.WriteLine("Test2: " + _uut2.Hash);
+            
+            //Den tjekker om Hash passer sammen
+            bool notSame = _uut2.Verify(_uut1.Salt, _uut2.Hash, "1234");
+            Assert.That(notSame,Is.EqualTo(false));
         }
 
         [Test]
