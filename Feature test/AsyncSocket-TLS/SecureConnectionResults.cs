7cef1d40cbcd0cab1c36aa97e94b24bce80215da
Martin Andersen <martin_haugaard8@hotmail.com>
2018-04-29 15:18:14 +0200

    Tilføjede asyncSocket projekter med TLS. Dette er outdated, så nyt forsøg laves nu oven i disse filer

---
+++ Feature test/AsyncSocket-TLS/SecureConnectionResults.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Security;
+
+public delegate void SecureConnectionResultsCallback(object sender, SecureConnectionResults args);
+
+public class SecureConnectionResults
+{
+    private SslStream secureStream;
+    private Exception asyncException;
+
+    internal SecureConnectionResults(SslStream sslStream)
+    {
+        this.secureStream = sslStream;
+    }
+
+    internal SecureConnectionResults(Exception exception)
+    {
+        this.asyncException = exception;
+    }
+
+    public Exception AsyncException { get { return asyncException; } }
+    public SslStream SecureStream { get { return secureStream; } }
+}
