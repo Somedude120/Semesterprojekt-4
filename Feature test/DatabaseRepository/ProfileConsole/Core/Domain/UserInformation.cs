427377fd238640ef10f728afca982ecf6495cb5a
Soeren Bech <soeren-bechy7@hotmail.com>
2018-05-08 11:17:40 +0200

    Redigerede IUnitofWork, samt tilføjet Status til UserInformation

--- Feature test/DatabaseRepository/ProfileConsole/Core/Domain/UserInformation.cs
+++ Feature test/DatabaseRepository/ProfileConsole/Core/Domain/UserInformation.cs
@@ -1,4 +1,4 @@
-namespace ProfileConsole
+namespace ProfileConsole.Core.Domain
 {
     using System;
     using System.Collections.Generic;
@@ -9,17 +9,6 @@ namespace ProfileConsole
     [Table("UserInformation")]
     public partial class UserInformation
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public UserInformation()
-        {
-            FriendList = new HashSet<FriendList>();
-            FriendList1 = new HashSet<FriendList>();
-            FriendList2 = new HashSet<FriendList>();
-            Chat = new HashSet<Chat>();
-            ChatGroups = new HashSet<ChatGroups>();
-            Tags = new HashSet<Tags>();
-        }
-
         [Key]
         [StringLength(15)]
         public string UserName { get; set; }
@@ -28,24 +17,7 @@ namespace ProfileConsole
         [StringLength(50)]
         public string Name { get; set; }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<FriendList> FriendList { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<FriendList> FriendList1 { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<FriendList> FriendList2 { get; set; }
-
-        public virtual Login Login { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Chat> Chat { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<ChatGroups> ChatGroups { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Tags> Tags { get; set; }
+        [StringLength(10)]
+        public string Status { get; set; }
     }
 }
