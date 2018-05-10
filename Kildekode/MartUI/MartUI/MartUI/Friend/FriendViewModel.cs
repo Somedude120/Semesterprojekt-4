5bfba71f2c7cb706df4c7ededbb7094eeb8a7702
Søren Mathiasen <zuperzam@email.dk>
2018-05-11 01:53:21 +0200

    Man kan nu tilføje sine højtelskede venner i MartUI

--- Kildekode/MartUI/MartUI/MartUI/Friend/FriendViewModel.cs
+++ Kildekode/MartUI/MartUI/MartUI/Friend/FriendViewModel.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Navigation;
 using MartUI.Chat;
 using MartUI.Events;
 using MartUI.Main;
@@ -20,23 +21,31 @@ namespace MartUI.Friend
     {
         private readonly IEventAggregator _eventAggregator;
         public string ReferenceName => "FriendViewModel"; // Returns "FriendViewModel"
-
         private ObservableCollection<FriendModel> _friendList;
         private FriendModel _selectedFriend;
         public ICommand ChooseFriendCommand { get; set; }
-        //public ICommand AddFriendCommand;
-        //public ICommand RemoveFriendCommand;
         private ICommand _addFriendCommand;
         private ICommand _removeFriendCommand;
-        private ICommand _doThreadedWorkCommand;
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                RaisePropertyChanged();
+            }
+
+        }
 
 
         public FriendViewModel()
         {
             _eventAggregator = GetEventAggregator.Get();
 
-            //FriendList = new List<FriendModel>();
             ChooseFriendCommand = new DelegateCommand<FriendModel>(SelectFriend);
+
+            Username = "Enter Username!";
 
             // Mulig løsning til når venner logger ind:
             // Subscribe på et event som serveren sender så man kan se når en ven logger ind
@@ -51,8 +60,6 @@ namespace MartUI.Friend
             //Skal bruge metode fra server/database til at få en liste af alle ens venner
             //Samt kun alle som er online 
         }
-
-        public ICommand DoThreadedWorkCommand => _doThreadedWorkCommand ?? (_doThreadedWorkCommand = new DelegateCommand(doSomething));
 
         // Mangler at tilføje en filtrering eller en anden liste som kun indeholder online venner
 
@@ -82,17 +89,17 @@ namespace MartUI.Friend
 
         public void AddFriend()
         {
-            for(int i = 0; i < 20; i++)
+            bool friendIntList = false;
+            foreach (var f in FriendList)
             {
-                Application.Current.Dispatcher.Invoke(new Action(() =>
+                if (f.Username == Username)
                 {
-                    FriendList.Add(new FriendModel {Username = "Yee" + i});
-                }));
+                    MessageBox.Show("This user is already on your friendlist");
+                    friendIntList = true;
+                }
             }
-            //if (!FriendList.Contains(_selectedFriend))
-            //    FriendList.Add(_selectedFriend);
-            //else
-            //    MessageBox.Show("This user is already on your friendlist!");
+            if(!friendIntList)
+                Application.Current.Dispatcher.Invoke(new Action(() => { FriendList.Add(new FriendModel { Username = Username }); }));
 
             //Skal kommunikere med database/server
         }
@@ -111,13 +118,6 @@ namespace MartUI.Friend
 
         public ICommand AddFriendCommand => _addFriendCommand ?? (_addFriendCommand = new DelegateCommand(AddFriend));
         public ICommand RemoveFriendCommand => _removeFriendCommand ?? (_removeFriendCommand = new DelegateCommand<FriendModel>(RemoveFriend));
-
-        private void doSomething()
-        {
-            Thread myThread = new Thread(AddFriend);
-            myThread.Start();
-        }
-
     }
 
     public class BindingProxy : Freezable
