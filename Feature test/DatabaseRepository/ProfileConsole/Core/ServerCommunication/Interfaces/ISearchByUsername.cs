6e5649ece1100874628e4bfc089b4b924aab6907
fred5954 <frederik.kastrup.mortensen@post.au.dk>
2018-05-16 15:44:55 +0200

    Tilføjet OtherProfile.cs og påbegyndt SearchByUsername

---
+++ Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/Interfaces/ISearchByUsername.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileConsole.Core.ServerCommunication.Interfaces
+{
+    public interface ISearchByUsername
+    {
+        void RequestUsername(String name);
+    }
+}
--- Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/SearchByUsername.cs
+++ Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/SearchByUsername.cs
@@ -1,6 +1,52 @@
-﻿namespace ProfileConsole.Core.ServerCommunication
+﻿using System;
+using System.Linq;
+using ProfileConsole.Core.Domain;
+using ProfileConsole.Persistence;
+
+namespace ProfileConsole.Core.ServerCommunication
 {
     public class SearchByUsername
     {
+        private IUnitOfWork unitOfWork;
+
+        public SearchByUsername()
+        {
+            unitOfWork = new UnitOfWork(new ProfileContext());
+        }
+
+        public OtherProfile RequestUsername(String name)
+        {
+            var person = unitOfWork.UserInformation.GetString(name);
+            if (person.Name == name)
+            {
+                using (var db = new ProfileContext())
+                {
+                    var profile =
+                    from p in db.UserInformation
+                    where p.UserName == name 
+                    select p;
+
+                    try
+                    {
+                        var otherprofile = new OtherProfile();
+                        
+
+                    }
+
+                    catch (Exception e)
+                    {
+                        return null;
+                        
+                    }
+                }
+                    
+            }
+
+            else
+            {
+
+            }
+           
+        }
     }
 }
