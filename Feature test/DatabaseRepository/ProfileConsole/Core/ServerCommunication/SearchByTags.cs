0652bf612a026052c467ebb4280550ec18697da6
Alexander Davidsen <alexander.l.davidsen@gmail.com>
2018-05-16 17:01:44 +0200

    SearchByTags, mangler at finde en løsning til returnering af OtherProfile

--- Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/SearchByTags.cs
+++ Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/SearchByTags.cs
@@ -1,7 +1,45 @@
-﻿namespace ProfileConsole.Core.ServerCommunication
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProfileConsole.Core.Domain;
+using ProfileConsole.Persistence;
+
+namespace ProfileConsole.Core.ServerCommunication
 {
     public class SearchByTags
     {
+        private IUnitOfWork unitOfWork;
 
+        public SearchByTags()
+        {
+            unitOfWork = new UnitOfWork(new ProfileContext());
+        }
+
+        public OtherProfile RequestTag(string tag)
+        {
+            var person = unitOfWork.Tags.GetString(tag);
+            if(person.TagName == tag)
+            using (var db = new ProfileContext())
+            {
+                var profile =
+                    from p in db.Tags
+                    where p.TagName == tag 
+                    select p;
+
+                try
+                {
+                    foreach (var pers in profile)
+                    {
+                        //return new OtherProfile(pers.UserName, pers.Description, pers.Tags);
+                    }
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
     }
 }
