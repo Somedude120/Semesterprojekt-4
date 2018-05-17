a27fa8f7861745884e938af3f13c5c61097bbdfe
fred5954 <frederik.kastrup.mortensen@post.au.dk>
2018-05-17 10:43:33 +0200

    Updateprofile og unit tests lavet, vil ikke opdatere description

--- Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/UpdateProfile.cs
+++ Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/UpdateProfile.cs
@@ -1,6 +1,54 @@
-﻿namespace ProfileConsole.Core.ServerCommunication
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProfileConsole.Core.Domain;
+using ProfileConsole.Core.ServerCommunication.Interfaces;
+using ProfileConsole.Persistence;
+
+namespace ProfileConsole.Core.ServerCommunication
 {
-    public class UpdateProfile
+    public class UpdateProfile : IUpdateProfile
     {
+        private IUnitOfWork unitOfWork;
+
+        public UpdateProfile() 
+        {
+            unitOfWork = new UnitOfWork(new ProfileContext());
+        }
+
+        public void UpdateProfileInformation(string Username, string Description, ICollection<Tags> Tagslist)
+        {
+            var person = unitOfWork.UserInformation.GetString(Username);
+
+            if (person.UserName == Username)
+            {
+                using (var db = new ProfileContext())
+                {
+                    var profile =
+                        from p in db.UserInformation
+                        where p.UserName == Username
+                        select p;
+
+                    try
+                    {
+                        foreach (var pers in profile)
+                        {
+                            pers.Description = Description;
+                            pers.Tags = Tagslist;
+                        }
+
+                        unitOfWork.Complete();
+                    }
+
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                }
+
+                
+            }
+        }
     }
 }
