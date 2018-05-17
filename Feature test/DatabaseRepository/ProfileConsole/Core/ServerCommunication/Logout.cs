53f94ee9a148b63ede47263ae63980729168d195
fred5954 <frederik.kastrup.mortensen@post.au.dk>
2018-05-17 17:07:25 +0200

    logout implementeret og testet

--- Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/Logout.cs
+++ Feature test/DatabaseRepository/ProfileConsole/Core/ServerCommunication/Logout.cs
@@ -1,7 +1,36 @@
-﻿namespace ProfileConsole.Core.ServerCommunication
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProfileConsole.Core.Domain;
+using ProfileConsole.Core.ServerCommunication.Interfaces;
+using ProfileConsole.Persistence;
+namespace ProfileConsole.Core.ServerCommunication
 {
-    public class Logout
+    public class Logout : ILogout
     {
+        private IUnitOfWork unitOfWork;
+
+        public Logout()
+        {
+            unitOfWork = new UnitOfWork(new ProfileContext());
+        }
+
+        public void LogoutDB(string Username)
+        {
+            var person = unitOfWork.UserInformation.GetString(Username);
+            using (var db = new ProfileContext())
+            {
+                var profile =
+                    from p in db.UserInformation
+                    where p.UserName == Username
+                    select p;
+
+                foreach (var p in profile)
+                {
+                    p.Status = "Offline";
+                }
+            }
+        }
 
     }
 }
