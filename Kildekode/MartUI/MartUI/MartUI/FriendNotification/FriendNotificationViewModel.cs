e81645bba5f18b10cabaae8971a0efacc27dd127
Søren Mathiasen <zuperzam@email.dk>
2018-05-21 14:22:44 +0200

    Tilføjet interface til FriendNotificationViewModel

--- Kildekode/MartUI/MartUI/MartUI/FriendNotification/FriendNotificationViewModel.cs
+++ Kildekode/MartUI/MartUI/MartUI/FriendNotification/FriendNotificationViewModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MartUI.Main;
+using Prism.Mvvm;
 
 namespace MartUI.FriendNotification
 {
-    class FriendNotificationViewModel
+    class FriendNotificationViewModel : BindableBase, IViewModel
     {
+        public string ReferenceName => "FriendNotification";
     }
 }
