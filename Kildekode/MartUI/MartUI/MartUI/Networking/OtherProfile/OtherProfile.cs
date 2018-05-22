8c4b6d71579b7ecc4e4c50ec677586fdf8c80ee7
Daniel Hansen <dph1108@gmail.com>
2018-05-22 13:06:13 +0200

    Tilføjet noget netværksstuff i form af Tags og OtherProfile

---
+++ Kildekode/MartUI/MartUI/MartUI/Networking/OtherProfile/OtherProfile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MartUI.Me;
+using ProfileConsole.Core.Domain;
+
+namespace ProfileConsole.Core.ServerCommunication
+{
+    public class OtherProfile
+    {
+        //Objectification of OtherProfile, ready for serialization
+        public MyData _userData;
+        public OtherProfile()
+        {
+
+            username = _userData.Username;
+            description = _userData.Description;
+            tags = _userData.Tags;
+            
+        }
+        public string username { get; set; }
+        public string description { get; set; }
+        public ICollection<string> tags { get; set; } 
+    }
+    
+}
