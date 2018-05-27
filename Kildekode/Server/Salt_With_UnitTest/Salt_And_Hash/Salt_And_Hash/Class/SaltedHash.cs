66a69856eafb3a10ab2487afe0f9037643380ea6
Martin Andersen <martin_haugaard8@hotmail.com>
2018-05-27 16:20:32 +0200

    Fjernede ubrugt kode

--- Kildekode/Server/Salt_With_UnitTest/Salt_And_Hash/Salt_And_Hash/Class/SaltedHash.cs
+++ Kildekode/Server/Salt_With_UnitTest/Salt_And_Hash/Salt_And_Hash/Class/SaltedHash.cs
@@ -8,22 +8,6 @@ namespace Salt_And_Hash
         public string Hash { get; private set; }
         public string Salt { get; private set; }
 
-        //public SaltedHash(string password)
-        //{
-        //    var saltBytes = new byte[32];
-        //    //Denne her randomizer min saltbytes
-        //    using (var provider = new RNGCryptoServiceProvider())
-        //        provider.GetNonZeroBytes(saltBytes);
-        //    //Output Salt og Hash
-        //    Salt = Convert.ToBase64String(saltBytes);
-        //    Hash = ComputeHash(Salt, password);
-        //}
-
-        public SaltedHash()
-        {
-
-        }
-
         public string MakeSalt()
         {
             var saltBytes = new byte[32];
@@ -32,43 +16,17 @@ namespace Salt_And_Hash
                 provider.GetNonZeroBytes(saltBytes);
             string salt = Convert.ToBase64String(saltBytes);
 
-                return salt;
+            return salt;
         }
-        
 
         public string ComputeHash(string salt, string password)
         {
-            //var saltBytes = Convert.FromBase64String(salt);
-            //using (var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 1000))
-            //    return Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
             string saltyHashPwd = salt + password;
             saltyHashPwd = GetStringSha256Hash(saltyHashPwd);
 
             return saltyHashPwd;
         }
 
-        public bool Verify(string salt, string hash, string password)
-        {
-            return hash == ComputeHash(salt, password);
-        }
-        //static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
-        //{
-        //    HashAlgorithm algorithm = new SHA256Managed();
-
-        //    byte[] plainTextWithSaltBytes =
-        //        new byte[plainText.Length + salt.Length];
-
-        //    for (int i = 0; i < plainText.Length; i++)
-        //    {
-        //        plainTextWithSaltBytes[i] = plainText[i];
-        //    }
-        //    for (int i = 0; i < salt.Length; i++)
-        //    {
-        //        plainTextWithSaltBytes[plainText.Length + i] = salt[i];
-        //    }
-
-        //    return algorithm.ComputeHash(plainTextWithSaltBytes);
-        //}
         public string GetStringSha256Hash(string text)
         {
             if (String.IsNullOrEmpty(text))
